8a599e04c540c7f2c5417b07eeaac3afa795aa20
Marcela Porto <marcela.fporto@gmail.com>
2017-05-31 11:51:05 -0700

    Polishes comments

--- kNN/kNN-algorithm.py
+++ kNN/kNN-algorithm.py
@@ -1,4 +1,9 @@
 
+# The test problem we will be using in this tutorial is iris classification.
+
+# The problem is comprised of 150 observations of iris flowers from three different species. There are 4 measurements of given flowers:  sepal length, sepal width, petal length and petal width, all in the same unit of centimeters. The predicted attribute is the species,  which is one of setosa, versicolor or virginica.
+
+
 ## HANDLE DATA
 
 #  load our data file
