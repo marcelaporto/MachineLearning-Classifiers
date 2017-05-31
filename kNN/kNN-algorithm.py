a3940aef4b7ad0631e2c2863169963fd8b979ec6
Marcela Porto <marcela.fporto@gmail.com>
2017-05-31 11:11:39 -0700

    Adds handle data function w/ comments

--- kNN/kNN-algorithm.py
+++ kNN/kNN-algorithm.py
@@ -1,8 +1,9 @@
+
+## HANDLE DATA
 #  load our data file
 # split the data into a training dataset and a test dataset
 # training dataset: kNN can use to make predictions
 # test dataset: used to evaluate the accuracy of the model.
-
 
 # loadDataset loads a CSV with the provided filename and splits it randomly into train and test datasets using the provided split ratio.
 import csv
@@ -11,6 +12,7 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	with open(filename, 'rb') as csvfile:
 	    lines = csv.reader(csvfile)
 	    dataset = list(lines)
+            print dataset
 	    for x in range(len(dataset)-1):
 	        for y in range(4):
 	            dataset[x][y] = float(dataset[x][y])
@@ -19,8 +21,13 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	        else:
 	            testSet.append(dataset[x])
 
-## DRIVER CODE ##
+## SIMILARITIES
+# In order to make predictions we need to calculate the similarity between any two given data instances.
 
+
+### DRIVER CODE ###
+
+## HANDLE DATA
 trainingSet=[]
 testSet=[]
 loadDataset('iris.data', 0.66, trainingSet, testSet)
